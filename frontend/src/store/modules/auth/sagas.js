4ad71544cd3fb9a95a44f7521f14fb189aa326a1
Marcos Willian <markusuuuu@gmail.com>
2019-10-23 03:20:14 -0300

    exibindo mensagens de erro de uma forma mais amigavel e loading basico adicionado na autenticação

--- frontend/src/store/modules/auth/sagas.js
+++ frontend/src/store/modules/auth/sagas.js
@@ -1,28 +1,37 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
-
+import { toast } from 'react-toastify';
 import api from '~/services/api';
 import history from '~/services/history';
 
-import { signInSuccess } from './actions';
+import { signInSuccess, signFailure } from './actions';
 
 export function* signIn({ payload }) {
-  const { email, password } = payload;
+  try {
+    const { email, password } = payload;
 
-  const response = yield call(api.post, 'sessions', {
-    email,
-    password,
-  });
+    const response = yield call(api.post, 'sessions', {
+      email,
+      password,
+    });
 
-  const { token, user } = response.data;
+    const { token, user } = response.data;
 
-  if (!user.organizer) {
-    console.tron.log('Usuário não possui um meetapp cadastrado');
-    return;
+    if (!user.organizer) {
+      toast.error('Usuário não possui um meetapp cadastrado');
+      yield put(signFailure());
+
+      return;
+    }
+
+    yield put(signInSuccess(token, user));
+
+    history.push('/dashboard');
+  } catch (error) {
+    toast.error(
+      'Parece que você digitou algum dado errado, verifique e tente novamente'
+    );
+    yield put(signFailure());
   }
-
-  yield put(signInSuccess(token, user));
-
-  history.push('/dashboard');
 }
 
 export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
