12ddcef1cce0ae3132a3eb03c2493497d2213c86
Marcos Willian <markusuuuu@gmail.com>
2019-11-07 08:21:30 -0300

    utilizando rate limit para limitar as requisições de todas as rotas

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,10 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import path from 'path';
+import redis from 'redis';
+import RateLimit from 'express-rate-limit';
+import RateLimitRedis from 'rate-limit-redis';
+
 import Youch from 'youch';
 import * as Sentry from '@sentry/node';
 import SentryConfig from './configs/sentry';
@@ -28,10 +32,26 @@ class App {
     this.server.use(cors());
     this.server.use(helmet());
     this.server.use(express.json());
+
     this.server.use(
       '/files',
       express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
     );
+
+    if (process.env.NODE_ENV !== 'development') {
+      this.server.use(
+        new RateLimit({
+          store: new RateLimitRedis({
+            client: redis.createClient({
+              host: process.env.REDIS_HOST,
+              port: process.env.REDIS_PORT,
+            }),
+          }),
+          windowMs: 1000 * 60 * 15,
+          max: 100,
+        })
+      );
+    }
   }
 
   routes() {
